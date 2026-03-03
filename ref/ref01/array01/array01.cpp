0b3c5632bc725b97ea6e524f6efd20eaefd1e874
karunamin <freedom12528@gmail.com>
2026-03-03 12:31:02 +0900

    커밋

---
+++ ref/ref01/array01/array01.cpp
@@ -0,0 +1,30 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	char name[20] = "홍길동";
+	char name1[20];
+	//cp_name = name1;
+	//name1 = "김영희";
+	strcpy(name1, "김영희");
+
+
+
+	cout << "name1: " << name1 << endl;
+	//cout << "cp_name " << endl;
+
+	char name2[20];
+	cout << "이름을 입력하세요: ";
+	cin >> name2;
+	cout << "당신의 이름은 " << name2 << "입니다" << endl;
+
+
+
+	return 0;
+
+}
---
+++ ref/ref01/ref02/ref02.cpp
@@ -0,0 +1,57 @@
+﻿
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int num = 10;
+//	int& ref = num;
+//	int* pnum = &num;
+//
+//	num++;
+//	ref++;
+//	(*pnum)++;
+//
+//	cout << "num: " << num << endl;
+//	cout << "ref: " << ref << endl;
+//	cout << "*pnum: " << *pnum << endl;
+//
+//	cout << "&num: " << &num << endl;
+//	cout << "&ref: " << &ref << endl;
+//
+//	return 0;
+//}
+
+
+#include <iostream>
+using namespace std;
+
+
+int funt(int n) {
+	n++;
+	return n;
+}
+
+int& funt1 (int r){
+r++;
+return r;
+
+}
+
+int main() {
+
+	int num = 10;
+
+	int ret = funt(num);
+	cout << ret << endl;
+
+	int res = funt1(num);
+	cout << res << endl;
+	int& ref = funt1(num);
+	cout << ref << endl;
+
+
+
+	return 0;
+}
